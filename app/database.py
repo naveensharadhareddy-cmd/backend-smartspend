77dc0761b80ce95159df1478272632d633dabdf9
NAVEEN-REDDY-MADALA <166785190+NAVEEN-REDDY-MADALA@users.noreply.github.com>
2026-05-11 21:20:45 +0530

    Simplify database URL loading and error check
    Refactor database connection setup and error handling.

--- app/database.py
+++ app/database.py
@@ -1,20 +1,9 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
-load_dotenv()
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
